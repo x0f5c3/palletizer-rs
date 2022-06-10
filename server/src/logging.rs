553b5b4d089cdb53cf1abde9687bf19b6fe56071
x0f5c3 <x0f5c3@tutanota.com>
2022-06-10 13:01:08 +0200

    Add flutter

--- server/src/logging.rs
+++ server/src/logging.rs
@@ -1,3 +1,4 @@
+use pretty_env_logger::env_logger::fmt::Color;
 use std::io::Write;
 
 /// Initialize the logging system with a pretty format.
@@ -5,77 +6,74 @@ use std::io::Write;
 /// Logging for the specified root module will be set to Error, Warn, Info, Debug or Trace, depending on the verbosity parameter.
 /// Logging for all other modules is set to one level less verbose.
 pub(crate) fn init(root_module: &str, extra_modules: &[&str], verbosity: i8) {
-	let log_level = match verbosity {
-		i8::MIN..=-2 => log::LevelFilter::Error,
-		-1 => log::LevelFilter::Warn,
-		0 => log::LevelFilter::Info,
-		1 => log::LevelFilter::Debug,
-		2..=i8::MAX => log::LevelFilter::Trace,
-	};
+    let log_level = match verbosity {
+        i8::MIN..=-2 => log::LevelFilter::Error,
+        -1 => log::LevelFilter::Warn,
+        0 => log::LevelFilter::Info,
+        1 => log::LevelFilter::Debug,
+        2..=i8::MAX => log::LevelFilter::Trace,
+    };
 
-	let extra_level = match verbosity {
-		i8::MIN..=-1 => log::LevelFilter::Error,
-		0 => log::LevelFilter::Warn,
-		1 => log::LevelFilter::Info,
-		2 => log::LevelFilter::Debug,
-		3..=i8::MAX => log::LevelFilter::Trace,
-	};
+    let extra_level = match verbosity {
+        i8::MIN..=-1 => log::LevelFilter::Error,
+        0 => log::LevelFilter::Warn,
+        1 => log::LevelFilter::Info,
+        2 => log::LevelFilter::Debug,
+        3..=i8::MAX => log::LevelFilter::Trace,
+    };
 
-	let mut logger = env_logger::Builder::new();
+    let mut logger = pretty_env_logger::formatted_builder();
 
-	logger.format(
-		move |buffer, record: &log::Record| {
-			use env_logger::fmt::Color;
+    logger.format(move |buffer, record: &log::Record| {
+        let now = time::OffsetDateTime::now_utc();
+        let mut date_style = buffer.style();
+        let mut time_style = buffer.style();
+        let mut target_style = buffer.style();
+        let mut level_style = buffer.style();
 
-			let now = chrono::Local::now();
-			let mut date_style = buffer.style();
-			let mut time_style = buffer.style();
-			let mut target_style = buffer.style();
-			let mut level_style = buffer.style();
+        date_style.set_color(Color::Cyan);
+        time_style.set_color(Color::Cyan);
+        target_style.set_color(Color::Magenta);
 
-			date_style.set_color(Color::Cyan);
-			time_style.set_color(Color::Cyan);
-			target_style.set_color(Color::Magenta);
+        let level;
+        match record.level() {
+            log::Level::Trace => {
+                level = "Trace";
+                level_style.set_bold(true);
+            }
+            log::Level::Debug => {
+                level = "Debug";
+                level_style.set_bold(true);
+            }
+            log::Level::Info => {
+                level = "Info";
+                level_style.set_bold(true);
+            }
+            log::Level::Warn => {
+                level = "Warn";
+                level_style.set_color(Color::Yellow).set_bold(true);
+            }
+            log::Level::Error => {
+                level = "Error";
+                level_style.set_color(Color::Red).set_bold(true);
+            }
+        };
+        let fmt1 = time::format_description::parse("%Y-%m-%d").unwrap();
+        let fmt2 = time::format_description::parse("%H:%M:%S%.3f").unwrap();
+        writeln!(
+            buffer,
+            "[{date} {time}] {level:<7} {message}",
+            date = date_style.value(now.format(&fmt1).unwrap()),
+            time = time_style.value(now.format(&fmt2).unwrap()),
+            level = format_args!("[{}]", level_style.value(level)),
+            message = record.args(),
+        )
+    });
 
-			let level;
-			match record.level() {
-				log::Level::Trace => {
-					level = "Trace";
-					level_style.set_bold(true);
-				},
-				log::Level::Debug => {
-					level = "Debug";
-					level_style.set_bold(true);
-				},
-				log::Level::Info => {
-					level = "Info";
-					level_style.set_bold(true);
-				},
-				log::Level::Warn => {
-					level = "Warn";
-					level_style.set_color(Color::Yellow).set_bold(true);
-				},
-				log::Level::Error => {
-					level = "Error";
-					level_style.set_color(Color::Red).set_bold(true);
-				},
-			};
+    logger.filter_module(root_module, log_level);
+    for module in extra_modules {
+        logger.filter_module(module, extra_level);
+    }
 
-			writeln!(
-				buffer,
-				"[{date} {time}] {level:<7} {message}",
-				date = date_style.value(now.format("%Y-%m-%d")),
-				time = time_style.value(now.format("%H:%M:%S%.3f")),
-				level = format_args!("[{}]", level_style.value(level)),
-				message = record.args(),
-			)
-		}
-	);
-
-	logger.filter_module(root_module, log_level);
-	for module in extra_modules {
-		logger.filter_module(module, extra_level);
-	}
-
-	logger.init();
+    logger.init();
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,11 @@
+#[macro_use]
+extern crate rocket;
+
+use clap::AppSettings;
+use clap::Parser;
 use palletizer::Registry;
 use std::path::PathBuf;
 use std::sync::{Arc, RwLock};
-use structopt::StructOpt;
-use structopt::clap::AppSettings;
 
 mod api_v1;
 mod config;
@@ -13,109 +16,139 @@ mod server;
 #[cfg(feature = "tls")]
 mod tls;
 
-#[derive(StructOpt)]
-#[structopt(setting = AppSettings::ColoredHelp)]
-#[structopt(setting = AppSettings::UnifiedHelpMessage)]
-#[structopt(setting = AppSettings::DeriveDisplayOrder)]
+#[derive(Parser)]
+#[clap(setting = AppSettings::ColoredHelp)]
+#[clap(setting = AppSettings::UnifiedHelpMessage)]
+#[clap(setting = AppSettings::DeriveDisplayOrder)]
 struct Options {
-	/// Show more messages. Pass twice for even more messages.
-	#[structopt(long, short)]
-	#[structopt(parse(from_occurrences))]
-	verbose: i8,
+    /// Show more messages. Pass twice for even more messages.
+    #[clap(long, short)]
+    #[clap(parse(from_occurrences))]
+    verbose: i8,
 
-	/// Show less messages. Pass twice for even less messages.
-	#[structopt(long, short)]
-	#[structopt(parse(from_occurrences))]
-	quiet: i8,
+    /// Show less messages. Pass twice for even less messages.
+    #[clap(long, short)]
+    #[clap(parse(from_occurrences))]
+    quiet: i8,
 
-	/// The configuration file to use.
-	config: PathBuf,
+    /// The configuration file to use.
+    config: PathBuf,
 }
 
 impl Options {
-	fn load_config(&self) -> Result<config::Config, ()> {
-		let data = std::fs::read(&self.config)
-			.map_err(|e| log::error!("Failed to read {}: {}", self.config.display(), e))?;
-		toml::from_slice(&data)
-			.map_err(|e| log::error!("Failed to parse {}: {}", self.config.display(), e))
-	}
+    fn load_config(&self) -> Result<config::Config, ()> {
+        let data = std::fs::read(&self.config)
+            .map_err(|e| log::error!("Failed to read {}: {}", self.config.display(), e))?;
+        toml::from_slice(&data)
+            .map_err(|e| log::error!("Failed to parse {}: {}", self.config.display(), e))
+    }
 }
 
 fn main() {
-	let options = Options::from_args();
-	logging::init(module_path!(), &[], options.verbose - options.quiet);
-	if let Err(()) = do_main(options) {
-		std::process::exit(1);
-	}
+    let options = Options::parse();
+    logging::init(module_path!(), &[], options.verbose - options.quiet);
+    if let Err(()) = do_main(options) {
+        std::process::exit(1);
+    }
 }
 
 fn do_main(options: Options) -> Result<(), ()> {
-	let config_dir = options.config.parent()
-		.ok_or_else(|| log::error!("Failed to determine parent directory of config file"))?;
-	let config = options.load_config()?;
-	let registry = Registry::open(config_dir.join(&config.registry))
-		.map_err(|e| log::error!("{}", e))?;
-	let index_repo_path = registry.index_dir();
-	let registry = Arc::new(RwLock::new(registry));
+    let config_dir = options
+        .config
+        .parent()
+        .ok_or_else(|| log::error!("Failed to determine parent directory of config file"))?;
+    let config = options.load_config()?;
+    let registry =
+        Registry::open(config_dir.join(&config.registry)).map_err(|e| log::error!("{}", e))?;
+    let index_repo_path = registry.index_dir();
+    let registry = Arc::new(RwLock::new(registry));
 
-	let runtime = tokio::runtime::Builder::new_multi_thread()
-		.enable_all()
-		.build()
-		.map_err(|e| log::error!("Failed to initialize I/O runtime: {}", e))?;
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .map_err(|e| log::error!("Failed to initialize I/O runtime: {}", e))?;
 
-	runtime.block_on(async move {
-		let mut futures = Vec::new();
-		for listener in config.listeners {
-			futures.push(run_server(registry.clone(), index_repo_path.clone(), config_dir.to_path_buf(), listener));
-		}
-		futures::future::try_join_all(futures).await?;
-		Ok(())
-	})
+    runtime.block_on(async move {
+        let mut futures = Vec::new();
+        for listener in config.listeners {
+            futures.push(run_server(
+                registry.clone(),
+                index_repo_path.clone(),
+                config_dir.to_path_buf(),
+                listener,
+            ));
+        }
+        futures::future::try_join_all(futures).await?;
+        Ok(())
+    })
 }
 
-async fn run_server(registry: Arc<RwLock<Registry>>, index_repo_path: PathBuf, config_dir: PathBuf, config: config::Listener) -> Result<(), ()> {
-	let listener = tokio::net::TcpListener::bind(&config.bind)
-		.await
-		.map_err(|e| log::error!("Failed to listen on {}: {}", &config.bind, e))?;
-	log::info!("Server listening on {}", config.bind);
+async fn run_server(
+    registry: Arc<RwLock<Registry>>,
+    index_repo_path: PathBuf,
+    config_dir: PathBuf,
+    config: config::Listener,
+) -> Result<(), ()> {
+    let listener = tokio::net::TcpListener::bind(&config.bind)
+        .await
+        .map_err(|e| log::error!("Failed to listen on {}: {}", &config.bind, e))?;
+    log::info!("Server listening on {}", config.bind);
 
-	#[cfg(feature = "tls")]
-	let mut tls_acceptor = match config.tls.as_ref() {
-		None => None,
-		Some(tls) => Some(tls::TlsAcceptor::from_config(tls, &config_dir)?),
-	};
+    #[cfg(feature = "tls")]
+    let mut tls_acceptor = match config.tls.as_ref() {
+        None => None,
+        Some(tls) => Some(tls::TlsAcceptor::from_config(tls, &config_dir)?),
+    };
 
-	loop {
-		let (connection, address) = listener.accept()
-			.await
-			.map_err(|e| log::error!("Failed to accept connection on {}: {}", &config.bind, e))?;
-		log::debug!("Accepted connection from {}", address);
+    loop {
+        let (connection, address) = listener
+            .accept()
+            .await
+            .map_err(|e| log::error!("Failed to accept connection on {}: {}", &config.bind, e))?;
+        log::debug!("Accepted connection from {}", address);
 
-		#[cfg(feature = "tls")]
-		if let Some(tls_acceptor) = &mut tls_acceptor {
-			let connection = tls_acceptor.accept(connection).await?;
-			tokio::spawn(serve_connection(connection, address, registry.clone(), index_repo_path.clone()));
-			continue;
-		}
+        #[cfg(feature = "tls")]
+        if let Some(tls_acceptor) = &mut tls_acceptor {
+            let connection = tls_acceptor.accept(connection).await?;
+            tokio::spawn(serve_connection(
+                connection,
+                address,
+                registry.clone(),
+                index_repo_path.clone(),
+            ));
+            continue;
+        }
 
-		tokio::spawn(serve_connection(connection, address, registry.clone(), index_repo_path.clone()));
-	}
+        tokio::spawn(serve_connection(
+            connection,
+            address,
+            registry.clone(),
+            index_repo_path.clone(),
+        ));
+    }
 }
 
-async fn serve_connection<S>(connection: S, address: std::net::SocketAddr, registry: Arc<RwLock<Registry>>, index_repo_path: PathBuf)
-where
-	S: tokio::io::AsyncRead + tokio::io::AsyncWrite + std::marker::Unpin + 'static,
+async fn serve_connection<S>(
+    connection: S,
+    address: std::net::SocketAddr,
+    registry: Arc<RwLock<Registry>>,
+    index_repo_path: PathBuf,
+) where
+    S: tokio::io::AsyncRead + tokio::io::AsyncWrite + Unpin + 'static,
 {
-	let result = hyper::server::conn::Http::new()
-		.serve_connection(connection, hyper::service::service_fn(move |request| {
-			server::handle_request(registry.clone(), index_repo_path.clone(), request)
-		}))
-		.await;
-	if let Err(e) = result {
-		let message = e.to_string();
-		// EEEW! But hyper forces us to do this :(
-		if !message.starts_with("error shutting down connection:") {
-			log::error!("Error in connection with {}: {}", address, message);
-		}
-	}
+    let result = hyper::server::conn::Http::new()
+        .serve_connection(
+            connection,
+            hyper::service::service_fn(move |request| {
+                server::handle_request(registry.clone(), index_repo_path.clone(), request)
+            }),
+        )
+        .await;
+    if let Err(e) = result {
+        let message = e.to_string();
+        // EEEW! But hyper forces us to do this :(
+        if !message.starts_with("error shutting down connection:") {
+            log::error!("Error in connection with {}: {}", address, message);
+        }
+    }
 }
